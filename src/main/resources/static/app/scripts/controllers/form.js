cc8706144476032e353487dc79b915aa27d7f93a
Maugin Thomas <tmaugin@sii.fr>
2015-04-15 17:15:20 +0200

    feat: sending button loader/disabled

--- src/main/resources/static/app/scripts/controllers/form.js
+++ src/main/resources/static/app/scripts/controllers/form.js
@@ -6,11 +6,13 @@ angular.module('CallForPaper')
 		$scope.formData = {};
 		$scope.formData.steps = {};
 		$scope.formData.steps.currentStep = 1;
+		$scope.formData.sending = false;
 		$scope.formData.steps.isValid = [false, false, false];
 		$scope.language = $translate.use();
 		// function to process the form
 
 		$scope.processForm = function(isValid) {
+			$scope.formData.sending = true;
 			var model = {};
 			angular.extend(model,$scope.formData.help);
 			angular.extend(model,$scope.formData.speaker);
@@ -21,11 +23,13 @@ angular.module('CallForPaper')
 			success(function(data, status, headers, config) {
 				// this callback will be called asynchronously
 				// when the response is available
+				$scope.formData.sending = false;
 				$state.go('form.result');
 			}).
 			error(function(data, status, headers, config) {
 				// called asynchronously if an error occurs
 				// or server returns response with an error status.
+				$scope.formData.sending = false;
 				$scope.sendError = true;
 			});
 
