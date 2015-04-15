4c543b911bfcccaa635cf4a7766dbe9365bec415
Maugin Thomas <tmaugin@sii.fr>
2015-04-15 13:54:42 +0200

    feat: webapp entry point

---
+++ src/main/java/fr/sii/rest/configuration/HomeController.java
@@ -0,0 +1,16 @@
+package fr.sii.rest.configuration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tmaugin on 15/04/2015.
+ */
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
+}
