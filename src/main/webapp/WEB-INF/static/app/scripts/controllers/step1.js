ab476f600dd9e6560d8988c449c7bb9bc8a20352
Maugin Thomas <tmaugin@sii.fr>
2015-04-23 15:41:28 +0200

    fix: phone input impossible to validate

--- src/main/webapp/WEB-INF/static/app/scripts/controllers/step1.js
+++ src/main/webapp/WEB-INF/static/app/scripts/controllers/step1.js
@@ -3,8 +3,9 @@
 angular.module('CallForPaper')
 	.controller('Step1Ctrl', function($scope) {
 	  $scope.$parent.formData.speaker = {};
+	  $scope.$parent.formData.speaker.phone = "";
 		$scope.$watch(function(){
-			return $scope.form.email.$valid && $scope.form.name.$valid && $scope.form.firstname.$valid && $scope.form.phone.$valid && $scope.form.company.$valid && $scope.form.bio.$valid && $scope.form.social.$valid;
+			return $scope.form.email.$valid && $scope.form.name.$valid && $scope.form.firstname.$valid && ($scope.form.phone.$valid || $scope.$parent.formData.speaker.phone == "") && $scope.form.company.$valid && $scope.form.bio.$valid && $scope.form.social.$valid;
 		},function(isValid){
 			$scope.$parent.formData.steps.isValid[0]= isValid;
 		})
