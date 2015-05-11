8f5d86a14bbda6c619af1aaa938765d30381c54b
Maugin Thomas <thomas.maugin@laposte.net>
2015-05-11 10:32:57 +0200

    chore: grunt build task
    use grunt-bower-task to run the build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,16 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     'use strict';
- 
+
     grunt.initConfig({
-        hub: {
-            all: {
-                src: ['src/main/webapp/WEB-INF/static/Gruntfile.js'],
-                tasks: ['build']
-            }
-        },
-        copy: {
-            node_modules: {
-                expand: true,
-                src: 'node_modules/**/*',
-                dest: 'src/main/webapp/WEB-INF/static/'
-            }
-        },
-        clean: {
-            app: {
-                src: ["src/main/webapp/WEB-INF/static/app/","src/main/webapp/WEB-INF/static/node_modules/"]
+        subgrunt: {
+            target0: {
+                projects: {
+                    'src/main/webapp/WEB-INF/static': 'build'
+                }
             }
         }
     });
- 
-    grunt.loadNpmTasks('grunt-hub');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.registerTask('build', ['copy:node_modules','hub','clean:app']);
+
+    grunt.loadNpmTasks('grunt-subgrunt');
+    grunt.registerTask('build', ['subgrunt']);
 }
