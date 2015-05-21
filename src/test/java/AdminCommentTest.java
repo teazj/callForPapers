81b7d074e4058ea4f698b50da339a2c94452bfba
Maugin Thomas <thomas.maugin@laposte.net>
2015-05-21 17:44:30 +0200

    test: add rest tests for admin rate/comment

--- src/test/java/AdminCommentTest.java
+++ src/test/java/AdminCommentTest.java
@@ -4,9 +4,11 @@
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 import fr.sii.domain.admin.comment.AdminComment;
 import fr.sii.service.admin.comment.AdminCommentService;
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -19,6 +21,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.HashMap;
+
+import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,14 +33,22 @@ import static org.junit.Assert.assertNotNull;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AdminCommentTest {
 
-    private final LocalServiceTestHelper helper =
-            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+    private LocalServiceTestHelper authHelper;
+
     @Autowired
     WebApplicationContext webApplicationContext;
 
     @Before public void
     setUp() {
-        helper.setUp();
+        HashMap<String, Object> envAttr = new HashMap<String, Object>();
+        envAttr.put("com.google.appengine.api.users.UserService.user_id_key", "6827685435");
+
+        //Setup the emulated Appengine environment
+        authHelper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100), new LocalUserServiceTestConfig())
+                .setEnvIsAdmin(true).setEnvIsLoggedIn(true).setEnvEmail("maugin.thomas@gmail.com").setEnvAuthDomain("google.com").setEnvAttributes(envAttr);
+
+        authHelper.setUp();
+
         RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
     }
 
@@ -43,7 +57,7 @@ public class AdminCommentTest {
 
     @After
     public void tearDown() {
-        helper.tearDown();
+        authHelper.tearDown();
     }
 
     @Test
@@ -54,7 +68,29 @@ public class AdminCommentTest {
         c.setComment("Lorem ipsum");
         c.setRowId(1L);
         AdminComment savedAdminComment = adminCommentService.save(c);
+
         assertNotNull(savedAdminComment);
+        assertEquals("Lorem ipsum", savedAdminComment.getComment());
+        assertEquals((Long) 1L,savedAdminComment.getUserId());
+        assertEquals((Long) 1L, savedAdminComment.getRowId());
         assertNotNull(savedAdminComment.getEntityId());
+    }
+
+    @Test
+    public void test2_postRest()
+    {
+        given()
+                .contentType("application/json")
+                .body("{\n" +
+                        "\"comment\" : \"Lorem ipsum\",\n" +
+                        "\"rowId\" : \"1\"\n" +
+                        "}")
+                .when()
+                .post("/api/admin/comment")
+                .then()
+                .statusCode(200)
+                .body("comment", Matchers.is("Lorem ipsum"))
+                .body("rowId", Matchers.is(1))
+                .body("userId", Matchers.is(68276854));
     }
 }
--- src/test/java/AdminRateTest.java
+++ src/test/java/AdminRateTest.java
@@ -4,9 +4,11 @@
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 import fr.sii.domain.admin.rate.AdminRate;
 import fr.sii.service.admin.rate.AdminRateService;
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -19,6 +21,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.HashMap;
+
+import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,14 +33,22 @@ import static org.junit.Assert.assertNotNull;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AdminRateTest {
 
-    private final LocalServiceTestHelper helper =
-            new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+    private LocalServiceTestHelper authHelper;
+
     @Autowired
     WebApplicationContext webApplicationContext;
 
     @Before public void
     setUp() {
-        helper.setUp();
+        HashMap<String, Object> envAttr = new HashMap<String, Object>();
+        envAttr.put("com.google.appengine.api.users.UserService.user_id_key", "6827685435");
+
+        //Setup the emulated Appengine environment
+        authHelper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100), new LocalUserServiceTestConfig())
+                .setEnvIsAdmin(true).setEnvIsLoggedIn(true).setEnvEmail("maugin.thomas@gmail.com").setEnvAuthDomain("google.com").setEnvAttributes(envAttr);
+
+        authHelper.setUp();
+
         RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
     }
 
@@ -43,7 +57,7 @@ public class AdminRateTest {
 
     @After
     public void tearDown() {
-        helper.tearDown();
+        authHelper.tearDown();
     }
 
     @Test
@@ -54,7 +68,29 @@ public class AdminRateTest {
         r.setRate(4);
         r.setRowId(1L);
         AdminRate savedAdminRate = adminRateService.save(r);
+
         assertNotNull(savedAdminRate);
+        assertEquals(4L, Long.parseLong(savedAdminRate.getRate().toString()));
+        assertEquals((Long) 1L,savedAdminRate.getUserId());
+        assertEquals((Long) 1L, savedAdminRate.getRowId());
         assertNotNull(savedAdminRate.getEntityId());
+    }
+
+    @Test
+    public void test2_postRest()
+    {
+        given()
+                .contentType("application/json")
+                .body("{\n" +
+                        "\"comment\" : \"Lorem ipsum\",\n" +
+                        "\"rowId\" : \"1\"\n" +
+                        "}")
+                .when()
+                .post("/api/admin/comment")
+                .then()
+                .statusCode(200)
+                .body("comment", Matchers.is("Lorem ipsum"))
+                .body("rowId", Matchers.is(1))
+                .body("userId", Matchers.is(68276854));
     }
 }
