8471b33110408b0cd3958376d526f2ed33971d86
Jean-Louis Jouannic <jeanlouis_jouannic@yahoo.fr>
2015-11-29 17:33:35 +0100

    [front] Removed third party satellizer.js from project files. Instead, rely on bower dependency.

--- src/main/static/app/scripts/controllers/login.js
+++ src/main/static/app/scripts/controllers/login.js
@@ -1,40 +1,39 @@
 'use strict';
 
-angular.module('CallForPaper')
-    .controller('LoginCtrl', ['$scope', '$auth', function($scope, $auth) {
-        $scope.loading = false;
-        $scope.alreadyLinked = false;
-        $scope.authenticate = function(provider) {
-            $scope.loading = true;
-            $auth.authenticate(provider).then(function() {
-                // Signed In.
+angular.module('CallForPaper').controller('LoginCtrl', ['$scope', '$auth', function($scope, $auth) {
+    $scope.loading = false;
+    $scope.alreadyLinked = false;
+    $scope.authenticate = function(provider) {
+        $scope.loading = true;
+        $auth.authenticate(provider).then(function() {
+            // Signed In.
+            $scope.$emit('authenticate');
+            $scope.loading = false;
+        }).catch(function(response) {
+            // Not signed In.
+            $scope.loading = false;
+            if (response.status === 409) {
+                $scope.alreadyLinked = true;
+            }
+        });
+    };
+
+    $scope.badCredentials = false;
+    $scope.login = function() {
+        $scope.loading = true;
+        $auth.login({
+                email: $scope.email,
+                password: $scope.password
+            })
+            .then(function() {
+                $scope.loading = false;
                 $scope.$emit('authenticate');
+            })
+            .catch(function(response) {
                 $scope.loading = false;
-            }).catch(function(response) {
-                // Not signed In.
-                $scope.loading = false;
-                if (response.status === 409) {
-                    $scope.alreadyLinked = true;
+                if (response.status === 401) {
+                    $scope.badCredentials = true;
                 }
             });
-        };
-
-        $scope.badCredentials = false;
-        $scope.login = function() {
-            $scope.loading = true;
-            $auth.login({
-                    email: $scope.email,
-                    password: $scope.password
-                })
-                .then(function() {
-                    $scope.loading = false;
-                    $scope.$emit('authenticate');
-                })
-                .catch(function(response) {
-                    $scope.loading = false;
-                    if (response.status === 401) {
-                        $scope.badCredentials = true;
-                    }
-                });
-        };
-    }]);
+    };
+}]);
