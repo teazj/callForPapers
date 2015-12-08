af0b16cb51bd4748dd45a8c61db3fc3d63b34dee
Alexandre Thomazo <alex@thomazo.info>
2015-12-08 20:52:47 +0100

    [back] using https gravatar image

--- src/main/java/fr/sii/service/GravatarUtils.java
+++ src/main/java/fr/sii/service/GravatarUtils.java
@@ -3,14 +3,12 @@ package fr.sii.service;
 
 import fr.sii.service.auth.MD5Utils;
 
-import java.security.MessageDigest;
-
 public final class GravatarUtils {
 
 	public static String getImageURL(String email) {
 		String hash = MD5Utils.md5Hex(email);
 
-		return "http://www.gravatar.com/avatar/".concat(hash).concat("?s=250");
+		return "https://www.gravatar.com/avatar/".concat(hash).concat("?s=250");
 	}
 
 }
