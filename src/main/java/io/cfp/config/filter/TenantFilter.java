55ae1c4f62b7af7b8716ce6547b74cc28a95f7b4
Nicolas De Loof <nicolas.deloof@gmail.com>
2016-05-03 19:46:14 +0200

    multi-tenancy
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ src/main/java/io/cfp/config/filter/TenantFilter.java
@@ -0,0 +1,26 @@
+package io.cfp.config.filter;
+
+import io.cfp.entity.Event;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class TenantFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String host = new URL(request.getRequestURL().toString()).getHost();
+
+        String tenant = host.substring(0, host.indexOf('.'));
+        Event.setCurrent(tenant);
+    }
+}
