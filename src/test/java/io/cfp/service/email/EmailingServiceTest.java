eb6675c1af31be31e0ead2f23f9e10464fb1c35b
Michel DAVID <mimah35@gmail.com>
2016-05-14 11:20:45 +0200

    Emailing cleanup

--- src/test/java/io/cfp/service/email/EmailingServiceTest.java
+++ src/test/java/io/cfp/service/email/EmailingServiceTest.java
@@ -21,6 +21,7 @@
 package io.cfp.service.email;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.anyMap;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -33,42 +34,38 @@ import static org.mockito.Mockito.when;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Properties;
 
-import io.cfp.dto.ApplicationSettings;
-import io.cfp.repository.CfpConfigRepo;
-import io.cfp.repository.EventRepository;
-import io.cfp.repository.UserRepo;
+import org.apache.velocity.app.VelocityEngine;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.ServerSetupTest;
 
-import io.cfp.ApplicationJUnit;
-import io.cfp.config.email.EmailingSettings;
-import io.cfp.config.global.GlobalSettings;
+import io.cfp.dto.ApplicationSettings;
 import io.cfp.dto.TalkAdmin;
 import io.cfp.dto.TalkUser;
 import io.cfp.dto.user.UserProfil;
 import io.cfp.entity.User;
+import io.cfp.repository.CfpConfigRepo;
+import io.cfp.repository.EventRepository;
+import io.cfp.repository.UserRepo;
 import io.cfp.service.admin.config.ApplicationConfigService;
-import io.cfp.service.admin.user.AdminUserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = EmailingServiceTest.Config.class)
@@ -82,19 +79,18 @@ public class EmailingServiceTest {
     private GreenMail testSmtp;
 
     @Autowired
-    ApplicationConfigService applicationConfigService;
+    private ApplicationConfigService applicationConfigService;
 
     @Autowired
     private JavaMailSenderImpl javaMailSender;
 
     @Autowired
-    private GlobalSettings globalSettings;
-
-    @Autowired
-    private EmailingSettings emailingSettings;
-
-    @Autowired
     private UserRepo users;
+    
+    @Autowired
+    private VelocityEngine velocityEngine;
+    
+    private String emailSender;
 
     private User user;
 
@@ -104,11 +100,8 @@ public class EmailingServiceTest {
 
     @Before
     public void setup() {
-        emailingService = new EmailingService(applicationConfigService,
-                                                globalSettings,
-                                                emailingSettings,
-                                                users,
-                                                javaMailSender);
+        emailingService = new EmailingService();
+        emailSender = "sender@cfp.io";
 
         user = new User();
         user.setId(1);
@@ -129,11 +122,12 @@ public class EmailingServiceTest {
 
         MockitoAnnotations.initMocks(this);
 
-        ReflectionTestUtils.setField(emailingService, "globalSettings", globalSettings);
-        ReflectionTestUtils.setField(emailingService, "emailingSettings", emailingSettings);
         ReflectionTestUtils.setField(emailingService, "users", users);
         ReflectionTestUtils.setField(emailingService, "javaMailSender", javaMailSender);
         ReflectionTestUtils.setField(emailingService, "applicationConfigService", applicationConfigService);
+        ReflectionTestUtils.setField(emailingService, "velocityEngine", velocityEngine);
+        ReflectionTestUtils.setField(emailingService, "emailSender", emailSender);
+        ReflectionTestUtils.setField(emailingService, "hostname", "demo.cfp.io");
 
         testSmtp = new GreenMail(ServerSetupTest.SMTP);
         testSmtp.start();
@@ -149,99 +143,92 @@ public class EmailingServiceTest {
     @Test
     public void sendSessionConfirmation() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.CONFIRMED, Locale.FRENCH);
-        String subject = emailingSettings.getSubject(EmailingSettings.EmailType.CONFIRMED, Locale.FRENCH);
+        String templatePath = emailingService.getTemplatePath("confirmed.html", Locale.FRENCH);
 
         // When
         emailingService.sendConfirmed(user, talkUser, Locale.FRENCH);
 
         // Then
-        verify(emailingService).processContent(eq(templatePath), anyMap());
-        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), eq(subject), anyString(), isNull(List.class), isNull(List.class));
+        verify(emailingService).processTemplate(eq(templatePath), anyMap());
+        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), anyString(), anyString(), isNull(List.class), isNull(List.class));
     }
 
     @Test
     public void sendNewCommentToSpeaker() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.NEW_COMMENT_TO_SPEAKER, Locale.FRENCH);
-        String subject = emailingSettings.getSubject(EmailingSettings.EmailType.NEW_COMMENT_TO_SPEAKER, Locale.FRENCH, talkAdmin.getName());
+    	String templatePath = emailingService.getTemplatePath("newMessage.html", Locale.FRENCH);
 
         // When
         emailingService.sendNewCommentToSpeaker(user, talkAdmin, Locale.FRENCH);
 
         // Then
-        verify(emailingService).processContent(eq(templatePath), anyMap());
-        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), eq(subject), anyString(), notNull(List.class), isNull(List.class));
+        verify(emailingService).processTemplate(eq(templatePath), anyMap());
+        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), anyString(), anyString(), notNull(List.class), isNull(List.class));
     }
 
     @Test
     public void sendNewMessageToAdmins() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.NEW_COMMENT_TO_ADMIN, Locale.FRENCH);
-        String subject = emailingSettings.getSubject(EmailingSettings.EmailType.NEW_COMMENT_TO_ADMIN, Locale.FRENCH,
-                user.getFirstname() + " " + user.getLastname(), talkUser.getName());
+    	String templatePath = emailingService.getTemplatePath("newMessageAdmin.html", Locale.FRENCH);
 
         // When
         emailingService.sendNewCommentToAdmins(user, talkUser, Locale.FRENCH);
 
         // Then
-        verify(emailingService).processContent(eq(templatePath), anyMap());
-        verify(emailingService).sendEmail(eq(emailingSettings.getEmailSender()), eq(subject), anyString(), isNull(List.class), notNull(List.class));
+        verify(emailingService).processTemplate(eq(templatePath), anyMap());
+        verify(emailingService).sendEmail(eq(emailSender), anyString(), anyString(), isNull(List.class), notNull(List.class));
     }
 
     @Test
     public void sendNotSelectionned() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.NOT_SELECTIONNED, Locale.FRENCH);
-        String subject = emailingSettings.getSubject(EmailingSettings.EmailType.NOT_SELECTIONNED, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("notSelectionned.html", Locale.FRENCH);
 
         // When
         emailingService.sendNotSelectionned(talkUser, Locale.FRENCH);
 
         // Then
-        verify(emailingService).processContent(eq(templatePath), anyMap());
-        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), eq(subject), anyString(), notNull(List.class), isNull(List.class));
+        verify(emailingService).processTemplate(eq(templatePath), anyMap());
+        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), anyString(), anyString(), notNull(List.class), isNull(List.class));
     }
 
     @Test
     public void sendPending() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.PENDING, Locale.FRENCH);
-        String subject = emailingSettings.getSubject(EmailingSettings.EmailType.PENDING, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("pending.html", Locale.FRENCH);
 
         // When
         emailingService.sendPending(talkUser, Locale.FRENCH);
 
         // Then
-        verify(emailingService).processContent(eq(templatePath), anyMap());
-        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), eq(subject), anyString(), notNull(List.class), isNull(List.class));
+        verify(emailingService).processTemplate(eq(templatePath), anyMap());
+        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), anyString(), anyString(), notNull(List.class), isNull(List.class));
     }
 
     @Test
     public void sendSelectionned() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.SELECTIONNED, Locale.FRENCH);
-        String subject = emailingSettings.getSubject(EmailingSettings.EmailType.SELECTIONNED, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("selectionned.html", Locale.FRENCH);
 
         // When
         emailingService.sendSelectionned(talkUser, Locale.FRENCH);
 
         // Then
-        verify(emailingService).processContent(eq(templatePath), anyMap());
-        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), eq(subject), anyString(), notNull(List.class), isNull(List.class));
+        verify(emailingService).processTemplate(eq(templatePath), anyMap());
+        verify(emailingService).sendEmail(eq(JOHN_DOE_EMAIL), anyString(), anyString(), notNull(List.class), isNull(List.class));
     }
 
     @Test
     public void processContentTest() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.TEST, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("test.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<>();
         map.put("var1", "test1");
         map.put("var2", "test2");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
@@ -250,27 +237,28 @@ public class EmailingServiceTest {
     @Test
     public void processContentConfirmed() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.CONFIRMED, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("confirmed.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("hostname", "http://yourappid.appspot.com/");
         map.put("id", "123");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @Test
     public void processContentNotSelectionned() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.NOT_SELECTIONNED, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("notSelectionned.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("community", "GDG Nantes");
@@ -278,18 +266,19 @@ public class EmailingServiceTest {
         map.put("hostname", "http://yourappid.appspot.com/");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @Test
     public void processContentPending() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.PENDING, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("pending.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("event", "DevFest 2015");
@@ -297,18 +286,19 @@ public class EmailingServiceTest {
         map.put("hostname", "http://yourappid.appspot.com/");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @Test
     public void processContentSelectionned() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.SELECTIONNED, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("selectionned.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("event", "DevFest 2015");
@@ -317,64 +307,68 @@ public class EmailingServiceTest {
         map.put("hostname", "http://yourappid.appspot.com/");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @Test
     public void processContentNewMessage() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.NEW_COMMENT_TO_SPEAKER, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("newMessage.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("hostname", "http://yourappid.appspot.com/");
         map.put("id", "123");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @Test
     public void processContentNewMessageAdmin() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.NEW_COMMENT_TO_ADMIN, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("newMessageAdmin.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("hostname", "http://yourappid.appspot.com/");
         map.put("id", "123");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @Test
     public void processContentSessionConfirmation() {
         // Given
-        String templatePath = emailingSettings.getTemplatePath(EmailingSettings.EmailType.CONFIRMED, Locale.FRENCH);
+    	String templatePath = emailingService.getTemplatePath("confirmed.html", Locale.FRENCH);
 
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "Thomas");
         map.put("talk", "Google App Engine pour les nuls");
         map.put("hostname", "http://yourappid.appspot.com/");
         map.put("id", "123");
 
         // When
-        String content = emailingService.processContent(templatePath, map);
+        String content = emailingService.processTemplate(templatePath, map);
 
         // Then
         assertEquals(false, content.contains("$"));
+        assertNotNull(map.get("subject"));
     }
 
     @After
@@ -409,16 +403,6 @@ public class EmailingServiceTest {
         }
 
         @Bean // field injection of EmailingService
-        public GlobalSettings globalSettings() {
-            return mock(GlobalSettings.class);
-        }
-
-        @Bean // field injection of EmailingService
-        public EmailingSettings emailingSettings() {
-            return new EmailingSettings();
-        }
-
-        @Bean // field injection of EmailingService
         public JavaMailSenderImpl javaMailSender() {
             return mock(JavaMailSenderImpl.class);
         }
@@ -432,7 +416,14 @@ public class EmailingServiceTest {
         public EventRepository eventRepo() {
             return mock(EventRepository.class);
         }
-
+        
+        @Bean
+        public VelocityEngine velocityEngine() {
+        	Properties props = new Properties();
+            props.setProperty("resource.loader", "class");
+            props.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            return new VelocityEngine(props);
+        }
 
     }
 }
